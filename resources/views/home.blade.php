c06a85bc80b0d810e8662e1c8936a9f695dd142f
GLCVLL <g.ioele96@outlook.it>
2023-08-02 16:50:14 +0200

    added data to site

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,15 @@
 <!-- resources/views/home.blade.php -->
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="it">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
 </head>
 <body>
-    <h1>Hello World</h1>
+    <h1>{{ $name }}</h1>
+    <p>Age: {{ $age }}</p>
+    <p>Occupation: {{ $occupation }}</p>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $data = [
+        'name' => 'Gioele Iacovella',
+        'age' => 27,
+        'occupation' => 'Web Developer',
+    ];
+
+    return view('home', $data);
 });
